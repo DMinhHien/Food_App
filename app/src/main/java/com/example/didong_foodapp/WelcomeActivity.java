8aeae73f62f9ba666796b78df854faa9da6c195c
Hien <duongminhhien14@gmail.com>
2024-05-09 10:05:37 +0700

    updated splash

--- app/src/main/java/com/example/didong_foodapp/WelcomeActivity.java
+++ app/src/main/java/com/example/didong_foodapp/WelcomeActivity.java
@@ -28,66 +28,11 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeActivity extends AppCompatActivity {
-    FusedLocationProviderClient LocationClient;
-    public static int REQUEST_PERMISSION_LOCATION=1;
-    SharedPreferences sharedPreferences;
 
-    @Override
-    public void onStart() {
-        sharedPreferences = getSharedPreferences("location",MODE_PRIVATE);
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
-                    REQUEST_PERMISSION_LOCATION);
-            return;
-        }
-        LocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-
-                            // Xử lý vị trí được trả về thành công
-                            float latitude = (float)location.getLatitude();
-                            float longitude =(float) location.getLongitude();
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            String sLatitude= String.valueOf(latitude);
-                            String sLongitude= String.valueOf(longitude);
-                            editor.putString("latitude",sLatitude);
-                            editor.putString("longitude",sLongitude);
-                            editor.commit();
-                            Log.d("LocationFood", "sLatitude: " + sLatitude + ", sLongitude: " + sLongitude);
-                        } else {
-                            // Không tìm thấy vị trí nào được trả về
-                            Log.e("LocationFood", "Không tìm thấy vị trí");
-                        }
-                    }
-                })
-                .addOnFailureListener(this, new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Xử lý lỗi khi yêu cầu vị trí
-                        Log.e("LocationFood", "Lỗi khi yêu cầu vị trí: " + e.getMessage());
-                    }
-                });
-//        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-//            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-//        }
-
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -95,9 +40,10 @@ public class WelcomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+        }
 //        FirebaseAuth.getInstance().signOut();
-        LocationClient = LocationServices.getFusedLocationProviderClient(this);
-
     }
 
 
