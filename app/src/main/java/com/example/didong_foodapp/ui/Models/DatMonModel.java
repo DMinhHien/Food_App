8008b48cad144a5dd7d7cdd99bd1cde54e6800e7
Duong <duongtx.t2.1922@gmail.com>
2024-05-12 17:14:54 +0700

    thay doi danh sach thuc don luu vao 1 list

---
+++ app/src/main/java/com/example/didong_foodapp/ui/Models/DatMonModel.java
@@ -0,0 +1,22 @@
+package com.example.didong_foodapp.ui.Models;
+
+public class DatMonModel {
+    String tenMonAn;
+    int soluong;
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public String getTenMonAn() {
+        return tenMonAn;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+
+    public void setTenMonAn(String tenMonAn) {
+        this.tenMonAn = tenMonAn;
+    }
+}
