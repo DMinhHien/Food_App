68e4e36ca1f7e48c93b56cff8c95b48238c389a0
Duong <duongtx.t2.1922@gmail.com>
2024-05-16 14:21:18 +0700

    commit nhé

--- app/src/main/java/com/example/didong_foodapp/ui/fragments/SavedFragment.java
+++ app/src/main/java/com/example/didong_foodapp/ui/fragments/SavedFragment.java
@@ -21,6 +21,7 @@ public class SavedFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_saved,container,false);
+
         return view;
 
 
